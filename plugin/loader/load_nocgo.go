5239bd5f38d3d054c294613f14740877838a5e56
Steven Allen <steven@stebalien.com>
2020-06-04 20:22:28 -0700

    feat: nice errors when failing to load plugins
    partially fixes #7305

---
+++ plugin/loader/load_nocgo.go
@@ -0,0 +1,17 @@
+// +build linux,!cgo,!noplugin darwin,!cgo,!noplugin
+
+package loader
+
+import (
+	"errors"
+
+	iplugin "github.com/ipfs/go-ipfs/plugin"
+)
+
+func init() {
+	loadPluginFunc = nocgoLoadPlugin
+}
+
+func nocgoLoadPlugin(fi string) ([]iplugin.Plugin, error) {
+	return nil, errors.New("not built with cgo support")
+}
